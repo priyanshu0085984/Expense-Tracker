9ad9b96fb50f9ba085052517d4859edcbb553a66
priyanshu0085984 <priyanshu0553.be20@chitkara.edu.in>
2022-12-26 14:22:39 +0530

    Debug

--- src/BaseAxios.js
+++ src/BaseAxios.js
@@ -2,7 +2,7 @@ import Axios from "axios";
 
 const axios = Axios.create({
   baseURL:
-    "http://expensetracker-env.eba-y3tvvutt.ap-northeast-1.elasticbeanstalk.com/",
+    "https://expensetracker-env.eba-y3tvvutt.ap-northeast-1.elasticbeanstalk.com/",
 });
 
 export default axios;
